c2b55208cdda794b4292435a7da4feca1407d7d8
Paulo M Rocha <paulo@ubuntao2.0>
2020-12-10 17:27:05 -0300

    enemy test

--- tests/testthread.cpp
+++ tests/testthread.cpp
@@ -4,19 +4,27 @@
 #include <time.h>
 #include <locale.h>
 #include <pthread.h>
+#include <semaphore.h>
+#include <bits/stdc++.h>
 #define LAR 30
 #define ALT 15
 #define UP 0
 #define DOWN 1
 #define LEFT 2
 #define RIGHT 3
-#define PLAYER_COLOR 2
 #define WALL_COLOR 1
+#define ENEMY_COLOR 2
+#define PLAYER_COLOR 3
+#define TIMER_COLOR 4
+
+using namespace std;
+
 // global
 int ch, op = 0;
-int x = 15, y = 7;
 int directions[4] = {0,1,2,3};
-
+int stop = 1;
+pair<int,int> p_coord;
+vector<pair<int,int>> e_coord;
 
 void rand_dir(){
   int values[4] = {0};
@@ -106,36 +114,60 @@ void print_map(){
   move(ALT+1, 0);
 
   for(int i = 0; i < LAR+2; i++) addch('#');
-  attroff(COLOR_PAIR(1));
+  attroff(COLOR_PAIR(WALL_COLOR));
   refresh();
   return;
 }
 
-void mov_player (int dir) {
-  delete_dir(y, x);
-  mvaddch(y, x, ' '); //erase last position;
+void mov_player(int dir) {
+  delete_dir(p_coord.second, p_coord.first);
+  mvaddch(p_coord.second, p_coord.first, ' '); //erase last position;
 
   if(dir == directions[0]){//DOWN
-    if (y > 1)
-      y--;
+    if (p_coord.second > 1)
+      p_coord.second--;
   }else if(dir == directions[1]){//UP
-    if(y < ALT)
-      y++;
+    if(p_coord.second < ALT)
+      p_coord.second++;
   }else if(dir == directions[2]){//LEFT
-    if(x > 1)
-      x--;
+    if(p_coord.first > 1)
+      p_coord.first--;
   }else if(dir == directions[3]){//RIGHT
-    if(x < LAR)
-      x++;
+    if(p_coord.first < LAR)
+      p_coord.first++;
   }
 //☬◈☫
-  attron(COLOR_PAIR(2));
-  mvaddstr(y, x, "◈");
-  attroff(COLOR_PAIR(2));
+  attron(COLOR_PAIR(PLAYER_COLOR));
+  mvaddstr(p_coord.second, p_coord.first, "◈");
+  attroff(COLOR_PAIR(PLAYER_COLOR));
   rand_dir();
-  print_dir(y, x);
+  print_dir(p_coord.second, p_coord.first);
   refresh();
   return;
+}
+
+void mov_enemy(){
+  //int dir_e = rand()%4;
+  //mvaddch(e_coord[0].second, e_coord[0].first, ' '); //erase last position;
+  e_coord[0].second ++;
+  // if(dir_e == 0){//DOWN
+  //   if (e_coord[0].second > 1)
+  //     e_coord[0].second--;
+  // }else if(dir_e == 1){//UP
+  //   if(e_coord[0].second < ALT)
+  //     e_coord[0].second++;
+  // }else if(dir_e == 2){//LEFT
+  //   if(e_coord[0].first > 1)
+  //     e_coord[0].first--;
+  // }else if(dir_e == 3){//RIGHT
+  //   if(e_coord[0].first < LAR)
+  //     e_coord[0].first++;
+  // }
+
+  attron(COLOR_PAIR(ENEMY_COLOR));
+  mvaddstr(e_coord[0].second, e_coord[0].first, "☫");
+  attroff(COLOR_PAIR(ENEMY_COLOR));
+  refresh();
 }
 
 void* player_thread(void* var){
@@ -166,16 +198,35 @@ void* player_thread(void* var){
       break;
     }
   }
+  stop = 0;
   pthread_exit(NULL);
 }
 
 void* timer_thread(void* var){
-  int time = 100;
-  while(time--){
-    move(ALT+3, 0);
-    printw("%d", time);
+  int sec=0, min=0;
+  while(stop){
+    attron(COLOR_PAIR(TIMER_COLOR));
+    move(0, 3);
+    printw("TIME - %02d:%02d", min, sec);
     refresh();
+    attroff(COLOR_PAIR(TIMER_COLOR));
+    sec++;
+    if(sec == 60){
+      sec = 0;
+      min++;
+    }
     sleep(1);
+  }
+  pthread_exit(NULL);
+}
+
+void* enemy_thread(void* var){
+  pair<int,int> ep;
+  ep.first = 5; ep.second = 8;
+  e_coord.push_back(ep);
+  while(stop){
+    mov_enemy();
+    sleep(2);
   }
   pthread_exit(NULL);
 }
@@ -190,9 +241,10 @@ int main (void) {
   //COLORS
   start_color();
   //        id            font color   bg color
-  init_pair(PLAYER_COLOR, COLOR_GREEN, COLOR_BLACK);//player
   init_pair(WALL_COLOR, COLOR_YELLOW, COLOR_YELLOW);//wall
-
+  init_pair(ENEMY_COLOR, COLOR_RED, COLOR_BLACK);
+  init_pair(PLAYER_COLOR, COLOR_GREEN, COLOR_BLACK);//player
+  init_pair(TIMER_COLOR, COLOR_WHITE, COLOR_BLACK);//timer
   //stops buffer
   cbreak();
   printw("Press any key to start\n"); /* Print init message   */
@@ -200,22 +252,29 @@ int main (void) {
   getch();
   curs_set(0);
   noecho();
+
+  p_coord.first = 15; p_coord.second = 7;
   print_map();
-  mov_player(' ');
+  mov_player(-1);
 
   //activate key code
   keypad(stdscr, TRUE);
 
   pthread_t timer;
   pthread_t player_t;
+  pthread_t enemy_t;
   void *status_timer;
   void *status_player;
-  pthread_create (&timer, NULL, timer_thread, NULL);
-  pthread_create (&player_t, NULL, player_thread, NULL);
+  void *status_enemy;
+
+  pthread_create(&timer, NULL, timer_thread, NULL);
+  pthread_create(&player_t, NULL, player_thread, NULL);
+  pthread_create(&enemy_t, NULL, enemy_thread, NULL);
   pthread_join(timer, &status_timer);
+  pthread_join(enemy_t, &status_enemy);
   pthread_join(player_t, &status_player);
 
-
+  //pthread_exit(NULL);
   endwin();                  /* End curses mode    */
   return 0;
 }
